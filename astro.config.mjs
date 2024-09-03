01d2c2881e63c9750054627a679bc6a2620870d7
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-09-02 22:00:17 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aluben-service.github.io',
+    site: 'https://aluben-service.duckdns.org',
 });
