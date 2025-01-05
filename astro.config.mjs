648bba624f1cad806e7ce544113c2ab1d5ccdf6a
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-01-05 09:40:55 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
+
+
+import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://aluben.duckdns.org',
+    plugins:[react(), tailwind({applyBaseStyles: false})]
 });
