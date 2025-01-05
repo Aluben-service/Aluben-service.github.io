809c065cdcc9bcd25b55feef5adef893a1ca22bd
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-01-05 09:38:49 -0800

    Update infinite-moving-cards.tsx

--- src/components/components/ui/infinite-moving-cards.tsx
+++ src/components/components/ui/infinite-moving-cards.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@components/lib/utils";
+import { cn } from "../../lib/utils";
 import React, { useEffect, useState } from "react";
 
 export const InfiniteMovingCards = ({
